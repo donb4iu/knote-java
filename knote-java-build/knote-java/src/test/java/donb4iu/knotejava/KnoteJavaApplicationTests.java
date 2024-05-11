34d2010c313b340b40c5b28015f7f20731bc5143
donb4iu <dbuddenbaum@gmail.com>
2024-05-10 22:58:06 -0400

    personal build updates

---
+++ knote-java-build/knote-java/src/test/java/donb4iu/knotejava/KnoteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package donb4iu.knotejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
